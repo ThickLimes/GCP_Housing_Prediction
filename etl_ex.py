d1dbf8607677ea1d24ad7a6a85fcc1048e8eab65
ThickLimes <77542388+ThickLimes@users.noreply.github.com>
2021-03-06 10:18:16 -0600

    changed to reflect raw data

--- etl_ex.py
+++ etl_ex.py
@@ -2,7 +2,7 @@ import pandas as pd
 import requests
 import io
 
-url = "REPLACE-THIS-WITH-THE-URL-OF-THE-CSV-FILE" # Make sure the url is the raw version of the file on GitHub
+url = "https://raw.githubusercontent.com/ThickLimes/GCP-application-chile/main/train.csv?token=ASPTH5CG4Y6CXGK6RB7A6T3AIOVVA" 
 download = requests.get(url).content
 df = pd.read_csv(io.StringIO(download.decode('utf-8')))
 test_len=len(df)
