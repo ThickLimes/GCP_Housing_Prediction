788e0f96c06eca73dc943a4c89a8ccbc3eab80a8
ThickLimes <77542388+ThickLimes@users.noreply.github.com>
2021-03-06 10:33:19 -0600

    Update test_etl_ex.py

--- test_etl_ex.py
+++ test_etl_ex.py
@@ -2,7 +2,7 @@ import pandas as pd
 import requests
 import io
 
-url = "https://raw.githubusercontent.com/ThickLimes/GCP-application-chile/main/train.csv?token=ASPTH5CG4Y6CXGK6RB7A6T3AIOVVA" 
+url = "https://raw.githubusercontent.com/ThickLimes/GCP-application-chile/main/train.csv" 
 download = requests.get(url).content
 df = pd.read_csv(io.StringIO(download.decode('utf-8')))
 test_len=len(df)
