3b0c9c00623165e96cd0fe6a6ce15e57bab25da6
ThickLimes <77542388+ThickLimes@users.noreply.github.com>
2021-03-06 10:31:05 -0600

    removed cred file for public facing

--- prediction_test.py
+++ prediction_test.py
@@ -3,18 +3,29 @@ import google.auth
 from google.oauth2 import service_account
 import pandas as pd
 import json
+import re
+import requests
 project_id = 'gcp-chili-project'
 compute_region = 'us-central1'
 model_display_name = 'backup_plan_20210227040744'
-#t=google.auth.load_credentials_from_file(r'C:\Users\logan\PycharmProjects\GCP-application-chile\gcp-chili-project-da83c37a90e5.json')
-t=service_account.Credentials.from_service_account_file(r'C:\Users\logan\PycharmProjects\GCP-application-chile\gcp-chili-project-da83c37a90e5.json')
+
+t=service_account.Credentials.from_service_account_file("ACC CRED HERE")
 model_id = 'backup_plan_20210227040744'
-f=open(r'C:\Users\logan\PycharmProjects\GCP-application-chile\test_ex2.json')
-inputs=json.load(f)
-print(inputs)
+
+
+
+url = 'https://github.com/caminofinancial/data-eng-take-home/blob/master/prequalresult.json'
+
+resp = requests.get(url)
+resp_parsed = re.sub(r'^jsonp\d+\(|\)\s+$', '', resp.text)
+inputs = json.load(resp_parsed)
+
 client = automl.TablesClient(project=project_id, region=compute_region, credentials=t)
 response = client.predict(model_display_name=model_display_name, inputs=inputs[0])
 
 print("Prediction results:")
 for result in response.payload:
     print("Predicted class name: {}".format(result.tables.value))
+    
+def test_prediction:
+    assert result.tables.value == 126043.5625
