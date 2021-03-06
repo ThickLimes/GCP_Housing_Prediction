788178fc7c69844396b738d46d550001543ce801
ThickLimes <77542388+ThickLimes@users.noreply.github.com>
2021-03-06 10:17:03 -0600

    Update etl_ex.py

--- etl_ex.py
+++ etl_ex.py
@@ -1,4 +1,9 @@
 import pandas as pd
-df=pd.read_csv('https://github.com/guga31bb/nflfastR-data/blob/master/data/play_by_play_2000.csv.gz?raw=True',compression='gzip', 
-        low_memory=False)
+import requests
+import io
+
+url = "REPLACE-THIS-WITH-THE-URL-OF-THE-CSV-FILE" # Make sure the url is the raw version of the file on GitHub
+download = requests.get(url).content
+df = pd.read_csv(io.StringIO(download.decode('utf-8')))
+test_len=len(df)
 print(df)
